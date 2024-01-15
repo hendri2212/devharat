6ae4e83fe823550ff31cc7d88dd88460e6b3b3a5
hendri2212 <cowok_cool320@yahoo.co.id>
2024-01-15 23:07:54 +0800

    migration add new migration that is schools and quizzes

--- database/migrations/2024_01_10_123431_create_quizzes_table.php
+++ database/migrations/2024_01_10_123431_create_quizzes_table.php
@@ -15,12 +15,15 @@ class CreateQuizzesTable extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('community_id');
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('community_id');
+            $table->unsignedBigInteger('school_id');
+            $table->string('class', 200);
             $table->timestamps();
 
-            $table->foreign('community_id')->references('id')->on('communities')->onDelete('restrict');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('school_id')->references('id')->on('schools')->onDelete('restrict');
+            $table->foreign('community_id')->references('id')->on('communities')->onDelete('restrict');
 
             // Add unique constraint for community_id and user_id
             $table->unique(['community_id', 'user_id']);
