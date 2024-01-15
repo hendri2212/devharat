7772828183014fb922a47dde2bd46d0efd0b7712
hendri2212 <cowok_cool320@yahoo.co.id>
2024-01-15 23:12:24 +0800

    create model Add new model and that is Idea, Quiz and School

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Quiz extends Model
+class Idea extends Model
 {
     use HasFactory;
     
-    public function community() {
-        return $this->belongsTo(Community::class);
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
