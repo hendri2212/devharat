b8919efa956c92d653a64b8a59f55ed1ad109c62
hendri2212 <cowok_cool320@yahoo.co.id>
2024-01-15 23:13:13 +0800

    login ui update ui login page, change button login and register position

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,18 +46,18 @@
             </div>
 
             {{-- <div class="flex items-center justify-end mt-4"> --}}
-            <div class="flex items-center justify-content-between mt-4">
+            <div class="flex items-center justify-content-between mt-4 mb-2">
                 <a href="/register" class="btn btn-info btn-sm text-white">DAFTAR</a>
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
             </div>
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
         </form>
     </x-auth-card>
 </x-guest-layout>
